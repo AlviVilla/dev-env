2786b6a7d9d94a9d2f12fa329ba14e53c363ff28
AlviVilla <a.villa6@hotmail.com>
2021-06-28 22:06:47 +0200

    New base dev-env

---
+++ provisioning/download-modules.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+git config --global user.name "$1"
+git config --global user.email "$2"
+
+# Allow clone with github as if we know the key already.
+# If this fails, GitHub has their keys compromised.
+cat /home/vagrant/githubKey >> /home/vagrant/.ssh/known_hosts
+rm /home/vagrant/githubKey
+
+# INSERT THE REPOSITORIES IN A LIST (separated by a single space) HERE:
+TEMPLATES=""
+LIBRARIES="" 
+SERVICES="er_storage_manager er_user_interface"
+BUILDING_BLOCKS=""
+REPOS="$TEMPLATES $LIBRARIES $SERVICES $BUILDING_BLOCKS"
+for i in $REPOS;
+do
+    git clone "git@github.com:AlviVilla/"$i".git" /home/vagrant/$i
+    cd /home/vagrant/$i && git flow init -d
+    cd /home/vagrant
+done
---
+++ provisioning/lib.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+# install python
+apt-get install python-dev python3-pip -q -y
+pip3 install virtualenv
+
+# Ensure file permissions
+chmod 600 /home/vagrant/.ssh/id_rsa
+
+# install git flow
+apt-get install git-flow -q -y
+
+# install net tools, ldap-utils (ie. netstat)
+apt install net-tools ldap-utils -q -y
+
+# install xorg, firefox
+apt install xorg firefox -q -y
